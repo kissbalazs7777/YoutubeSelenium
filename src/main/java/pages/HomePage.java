1f0543b9192c530c5fa2ba37abe21a3f94c0a5da
kissbalazs7777 <73421236+kissbalazs7777@users.noreply.github.com>
2021-07-21 23:31:22 +0200

    v1.3

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -5,13 +5,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 public class HomePage {
 
     private WebDriver driver;
     private final By languageSettings  = By.xpath("//div[25]/span");
     private final By huOptionFromLanguageSettings = By.xpath("//div[@data-value='hu']");
     private final By emailInput = By.name("identifier");
-    private final By nextButton = By.xpath("//span[text()='Következő']");
+    private final By nextButton = By.xpath("//div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span");
     private final By passwordInput = By.name("password");
     private final By submitButton = By.id("passwordNext");
 
@@ -20,6 +22,7 @@ public class HomePage {
     }
 
     public void clickHuOption(){
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.findElement(huOptionFromLanguageSettings).click();
     }
 
